452a2eaeb4605e62ef1a5b681ca4e0264aabc939
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2016-08-27 20:59:30 -0400

    fix type in types import in course reducer

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,4 +1,4 @@
-import * types from '../actions/actionTypes';
+import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state = [], action) {
   switch(action.type) {
