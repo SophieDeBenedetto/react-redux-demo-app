107dccbe1d4aa8d4f15b4e2af2e51b7b4c7a060e
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2016-08-27 20:57:59 -0400

    use constants for action types

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,8 @@
+import * types from '../actions/actionTypes';
+
 export default function courseReducer(state = [], action) {
   switch(action.type) {
-    case 'CREATE_COURSE':
+    case types.CREATE_COURSE:
       // don't mutate state!!!
         // state.push(action.course);
         // return state;
