a60f3c1baa40d2cea66a100ddfb3c16c4641428c
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-08-29 14:51:24 -0400

    production build

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -28,13 +28,14 @@ export default function courseReducer(state = initialState.courses, action) {
         ...state.filter(course => course.id !== action.course.id),
         Object.assign({}, action.course)
       ];
-    case types.DELETE_COURSE_SUCCESS:
-      let newState = Object.assign([], state);
+    case types.DELETE_COURSE_SUCCESS: {
+      const newState = Object.assign([], state);
       const indexOfCourseToDelete = state.findIndex(course => {
           course.courseId == action.course.Id;
         });
       newState.splice(indexOfCourseToDelete, 1);
       return newState;
+    }
     default: 
       return state;
   }
